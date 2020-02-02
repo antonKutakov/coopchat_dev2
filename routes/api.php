192931fb38d7d2495df30a7f0f9f89947f94605a
antonKutakov <antonkutakov21@gmail.com>
2020-02-02 04:20:18 +0200

    f

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/send', 'Api/V1/MessageController@store');
+Route::post('/api/v1/send', 'Api/V1/MessageController@store');
